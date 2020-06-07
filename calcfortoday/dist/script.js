a7f1d7a1f7c92497e122dca5e8c1c554c023d301
SteveJayH <jay0118@naver.com>
2020-06-07 23:54:42 +0900

    d

--- calcfortoday/dist/script.js
+++ calcfortoday/dist/script.js
@@ -1,37 +1,88 @@
 //Calculate Tip
-function calculateTip() {
-  var billAmt = document.getElementById("billamt").value;
-  var serviceQual = document.getElementById("serviceQual").value;
-  var numOfPeople = document.getElementById("peopleamt").value;
+var price = 0.0;
+var month = 0;
 
-  //validate input
-  if (billAmt === "" || serviceQual == 0) {
-    alert("Please enter values");
-    return;
+function p1_click(){
+  var replacedString = "선택 : 100,000 원"
+  price = 100000;
+  document.getElementsByTagName("h4")[0].innerHTML = replacedString;
+}
+function p2_click(){
+  var replacedString = "선택 : 1,000,000 원"
+  price = 1000000;
+  document.getElementsByTagName("h4")[0].innerHTML = replacedString;
+}
+function p3_click(){
+  var replacedString = "선택 : 5,000,000 원"
+  price = 5000000;
+  document.getElementsByTagName("h4")[0].innerHTML = replacedString;
+}
+function p4_click(){
+  function updateValue(e){
+    price = Number(e.target.value) * 10000;
+    amount = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    document.getElementsByTagName("h4")[0].innerHTML = "선택 : " + amount + " 원";
   }
-  //Check to see if this input is empty or less than or equal to 1
-  if (numOfPeople === "" || numOfPeople <= 1) {
-    numOfPeople = 1;
-    document.getElementById("each").style.display = "none";
-  } else {
-    document.getElementById("each").style.display = "block";
+  const input = document.getElementById("billamt1");
+  input.addEventListener('input', updateValue);
+}
+function p5_click(){
+  var replacedString = "선택 : 1달"
+  month = 1;
+  document.getElementsByTagName("h4")[1].innerHTML = replacedString;
+}
+function p6_click(){
+  var replacedString = "선택 : 1년"
+  month = 12;
+  document.getElementsByTagName("h4")[1].innerHTML = replacedString;
+}
+function p7_click(){
+  var replacedString = "선택 : 5년"
+  month = 60;
+  document.getElementsByTagName("h4")[1].innerHTML = replacedString;
+}
+function p8_click(){
+  function updateValue(e){
+    month = Number(e.target.value);
+    n_year = Math.floor(month/12);
+    n_month = (month % 12).toString();
+    amount = month.toString();
+    if(n_year>=1){
+      document.getElementsByTagName("h4")[1].innerHTML = "선택 : " + String(n_year) +"년 " + String(n_month) + "달";
+    }
+    else{
+      document.getElementsByTagName("h4")[1].innerHTML = "선택 : " + amount + "달";
+    }
+    
   }
+  const input = document.getElementById("billamt2");
+  input.addEventListener('input', updateValue);
+}
+function onlyNumber(){
+  if((event.keyCode<48)||(event.keyCode>57))
+    event.returnValue=false;
+}
+
+function calculateTip() {
+
+  // alert("Please enter values");
+  // return;
 
   //Calculate tip
-  var total = (billAmt * serviceQual) / numOfPeople;
+  var total = 0.0;
   //round to two decimal places
-  total = Math.round(total * 100) / 100;
+  total = Math.round(price) / (month*31);
   //next line allows us to always have two digits after decimal point
-  total = total.toFixed(2);
+  total = total.toFixed(0);
+  total = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   //Display the tip
   document.getElementById("totalTip").style.display = "block";
-  document.getElementById("tip").innerHTML = total;
-
+  document.getElementById("tip").innerHTML = total + "원";
 }
 
 //Hide the tip amount on load
 document.getElementById("totalTip").style.display = "none";
-document.getElementById("each").style.display = "none";
+
 
 //click to call function
 document.getElementById("calculate").onclick = function() {
